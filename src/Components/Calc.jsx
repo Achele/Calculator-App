29794e86d1bbf230be0e016212000494154f2270
Ojoachele Onuh <ejigboojoachele@gmail.com>
2022-10-07 12:02:50 +0100

    Psuedo coding my logic

---
+++ src/Components/Calc.jsx
@@ -0,0 +1,11 @@
+// === PSUEDO CODE ===
+// import useState
+// create a function for calculator
+// define and set different usestates. such as, for when an operator is clicked, operator clicked should be false.
+// set state for two different values or number input. that should be empty
+// the state for numbers to be displayed should be set to a zero.
+// create a function to handle display of numbers
+// use a conditional statement to identify the different cases for there to be a number displayed
+// create a function that handles the different mathematical operators
+// create a function to handle the calculations such as for addition, subtration etc.
+// create a function to clear the operations.
