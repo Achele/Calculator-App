93b4e7341743085b5dcbc5efa4c867a53dcb6254
Ojoachele Onuh <101713829+Achele@users.noreply.github.com>
2022-10-08 02:14:41 +0000

    corrected some typographical errors

--- src/Components/Calc.jsx
+++ src/Components/Calc.jsx
@@ -14,13 +14,13 @@ import { useState } from "react";
 
 
 export default function Calc() {
-    const [operatorCLicked, setOperatorClicked]= false
-    const[secondValue, setSecondValue] = ""
-    const [operator, setOperator] = null
-    const[displayNum, serDisplayNum] = useState('0')
+  const [operatorClicked, setOperatorClicked] = useState(false)
+  const [secondValue, setSecondValue] = useState("")
+  const [operator, setOperator] = useState(null)
+  const [displayNum, setDisplayNum] = useState('0')
 
 
-    //display numbers clicked by users in the display screen
+  //display numbers clicked by users in the display screen
   const handleDisplay = (e) => {
     if (operatorClicked === false) {
       if (displayNum === "0" && e.target.value !== ".") {
@@ -33,123 +33,123 @@ export default function Calc() {
         setDisplayNum((prev) => prev + e.target.value)
       }
     } else if (operatorClicked === true) {
-        setSecondValue(displayNum)
-        setDisplayNum(e.target.value)
-        setOperatorClicked(false)
-      }
-    }
-  
-    // Handles the calculator operators(+, -, =, *, /)
-    const handleOperator = (e) => {
-      if (e.target.value === "=") {
-        handleCal()
-        setSecondValue(0)
-        setOperator(null)
-        // console.log(operator, secondValue, displayNum)
-  
-      }
-      else {
-        setOperatorClicked(true)
-        setOperator(e.target.value)
-        console.log(operator, secondValue, displayNum)
-        if (operator !== null) {
-          handleCal()
-          setSecondValue(0)
-        //   console.log(operator, secondValue, displayNum)
-        }
-      }
-    }
-  
-    // Handles the main functionality of the calculator
-    const handleCal = () => {
-  
-      if (operator === "+") {
-        return setDisplayNum(`${Number(secondValue) + Number(displayNum)}`)
-  
-      } else if (operator === "-") {
-        return setDisplayNum(`${Number(secondValue) - Number(displayNum)}`)
-  
-      } else if (operator === "x") {
-        return setDisplayNum(`${Number(secondValue) * Number(displayNum)}`)
-  
-      } else if (operator === "/") {
-        return setDisplayNum(`${Number(secondValue) / Number(displayNum)}`)
-      }
-  
-    }
-  
-    // this resets the calculator
-    const handleClear = () => {
-      setDisplayNum("0")
-      setOperator(null)
-      setSecondValue("")
+      setSecondValue(displayNum)
+      setDisplayNum(e.target.value)
       setOperatorClicked(false)
     }
-  
-    return (
-      <>
-        <section className="container">
-          <section className="header">
-            <h3>Achez calculator</h3>
-          </section>
-  
-          <section className="screen-container">
-            <div className='main-display'>
-              {displayNum}
-            </div>
-          </section>
-  
-          <section className='buttons'>
-            <div className="flex-container">
-              <button className="on" value="7" onClick={(e) => handleDisplay(e)}>7</button>
-              <button className="on" value="8" onClick={(e) => handleDisplay(e)}>8</button>
-              <button className="on" value="9" onClick={(e) => handleDisplay(e)}>9</button>
-              <button className="orange"
-                value="/"
-                onClick={(e) => handleOperator(e)}>&divide;</button>
-            </div>
-  
-            <div className="flex-container">
-              <button className="darkgray-btn" value="4"
-                onClick={(e) => handleDisplay(e)}>4</button>
-              <button className="darkgray-btn" value="5"
-                onClick={(e) => handleDisplay(e)}>5</button>
-              <button className="darkgray-btn" value="6"
-                onClick={(e) => handleDisplay(e)}>6</button>
-              <button className="orange"
-                value="x"
-                onClick={(e) => handleOperator(e)}
-              >x</button>
-            </div>
-  
-            <div className="flex-container">
-              <button className="darkgray-btn" value="1"
-                onClick={(e) => handleDisplay(e)}>1</button>
-              <button className="darkgray-btn" value="2"
-                onClick={(e) => handleDisplay(e)}>2</button>
-              <button className="darkgray-btn" value="3"
-                onClick={(e) => handleDisplay(e)}>3</button>
-              <button className="orange"
-                value="-"
-                onClick={(e) => handleOperator(e)}>-</button>
-            </div>
-  
-            <div className="flex-container">
-              <button id="zero" className="darkgray-btn" value="0"
-                onClick={(e) => handleDisplay(e)}>0</button>
-              <button className="darkgray-btn" value="."
-                onClick={(e) => handleDisplay(e)}
-              >.</button>
-              <button className="orange" value="+" onClick={(e) => handleOperator(e)}>+</button>
-            </div>
-  
-            <div className="flex-container">
-              <button id="clear" className="darkgray-btn" onClick={(e) => handleClear(e)}>C</button>
-              <button className="orange" value="=" onClick={handleOperator}>=</button>
-            </div>
-          </section>
+  }
+
+  // Handles the calculator operators(+, -, =, *, /)
+  const handleOperator = (e) => {
+    if (e.target.value === "=") {
+      handleCal()
+      setSecondValue(0)
+      setOperator(null)
+      // console.log(operator, secondValue, displayNum)
+
+    }
+    else {
+      setOperatorClicked(true)
+      setOperator(e.target.value)
+      console.log(operator, secondValue, displayNum)
+      if (operator !== null) {
+        handleCal()
+        setSecondValue(0)
+        //   console.log(operator, secondValue, displayNum)
+      }
+    }
+  }
+
+  // Handles the main functionality of the calculator
+  const handleCal = () => {
+
+    if (operator === "+") {
+      return setDisplayNum(`${Number(secondValue) + Number(displayNum)}`)
+
+    } else if (operator === "-") {
+      return setDisplayNum(`${Number(secondValue) - Number(displayNum)}`)
+
+    } else if (operator === "x") {
+      return setDisplayNum(`${Number(secondValue) * Number(displayNum)}`)
+
+    } else if (operator === "/") {
+      return setDisplayNum(`${Number(secondValue) / Number(displayNum)}`)
+    }
+
+  }
+
+  // this resets the calculator
+  const handleClear = () => {
+    setDisplayNum("0")
+    setOperator(null)
+    setSecondValue("")
+    setOperatorClicked(false)
+  }
+
+  return (
+    <>
+      <section className="container">
+        <section className="header">
+          <h3>Achez calculator</h3>
         </section>
-      </>
-  
-    )
+
+        <section className="screen-container">
+          <div className='main-display'>
+            {displayNum}
+          </div>
+        </section>
+
+        <section className='buttons'>
+          <div className="flex-container">
+            <button className="on" value="7" onClick={(e) => handleDisplay(e)}>7</button>
+            <button className="on" value="8" onClick={(e) => handleDisplay(e)}>8</button>
+            <button className="on" value="9" onClick={(e) => handleDisplay(e)}>9</button>
+            <button className="orange"
+              value="/"
+              onClick={(e) => handleOperator(e)}>&divide;</button>
+          </div>
+
+          <div className="flex-container">
+            <button className="darkgray-btn" value="4"
+              onClick={(e) => handleDisplay(e)}>4</button>
+            <button className="darkgray-btn" value="5"
+              onClick={(e) => handleDisplay(e)}>5</button>
+            <button className="darkgray-btn" value="6"
+              onClick={(e) => handleDisplay(e)}>6</button>
+            <button className="orange"
+              value="x"
+              onClick={(e) => handleOperator(e)}
+            >x</button>
+          </div>
+
+          <div className="flex-container">
+            <button className="darkgray-btn" value="1"
+              onClick={(e) => handleDisplay(e)}>1</button>
+            <button className="darkgray-btn" value="2"
+              onClick={(e) => handleDisplay(e)}>2</button>
+            <button className="darkgray-btn" value="3"
+              onClick={(e) => handleDisplay(e)}>3</button>
+            <button className="orange"
+              value="-"
+              onClick={(e) => handleOperator(e)}>-</button>
+          </div>
+
+          <div className="flex-container">
+            <button id="zero" className="darkgray-btn" value="0"
+              onClick={(e) => handleDisplay(e)}>0</button>
+            <button className="darkgray-btn" value="."
+              onClick={(e) => handleDisplay(e)}
+            >.</button>
+            <button className="orange" value="+" onClick={(e) => handleOperator(e)}>+</button>
+          </div>
+
+          <div className="flex-container">
+            <button id="clear" className="darkgray-btn" onClick={(e) => handleClear(e)}>C</button>
+            <button className="orange" value="=" onClick={handleOperator}>=</button>
+          </div>
+        </section>
+      </section>
+    </>
+
+  )
 }
