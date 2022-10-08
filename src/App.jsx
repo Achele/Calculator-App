268d23cabbb904e36d78c5b08c52fa237fcb9b74
Ojoachele Onuh <ejigboojoachele@gmail.com>
2022-10-08 02:28:26 +0100

    Created a footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Calc from './Components/Calc.jsx'
+import Footer from './Components/Footer'
 
 export default function App() {
   return (
     <main>
      <Calc />
+     <Footer />
     </main>
   )
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+    return(
+        <footer className="attribution">
+            Challenge by <a href="https://www.altschoolafrica.com/" target="_blank">Altschool Africa</a>
+            Coded by <a href="https://github.com/Achele" target="_blank">Ojoachele Onuh</a>
+        </footer>
+    )
+}
